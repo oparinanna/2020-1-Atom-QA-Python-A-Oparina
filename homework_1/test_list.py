c7139c0850204852c8817dc201054d835adc2af4
Oparina Anna <oparinanna@yandex.ru>
2020-03-07 00:32:57 +0300

    Добавила пробелы после запятой, почистила темплейт, создала файл requirements.txt.

--- homework_1/test_list.py
+++ homework_1/test_list.py
@@ -2,15 +2,15 @@ import pytest
 
 class TestList():
     def test_sum_lists(self):
-        a = [1,2,3]
+        a = [1, 2, 3]
         b = [4]
-        assert a + b == [1,2,3,4]
+        assert a + b == [1, 2, 3, 4]
 
     def test_list_length(self):
         assert len(['a'] * 6) == 6
 
     def test_delete_element(self):
-        a = [1,2,3,4,5,6]
+        a = [1, 2, 3, 4, 5, 6]
         a.pop(2)
         assert a[2] != 3
 
@@ -19,6 +19,6 @@ class TestList():
         a.append(1)
         assert a != []
 
-@pytest.mark.parametrize('x,y', [([1],[1]), ([1,1], [1,1]), (['a', 'b'], ['b', 'a']), ([1, '1'], ['1', 1])])
+@pytest.mark.parametrize('x,y', [([1], [1]), ([1, 1], [1, 1]), (['a', 'b'], ['b', 'a']), ([1, '1'], ['1', 1])])
 def test_reverse_list(x, y):
     assert x == y[::-1]
